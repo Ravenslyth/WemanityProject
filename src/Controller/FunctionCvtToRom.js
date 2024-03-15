923989665ec6b633d2a13a0f95aa7e42c8469280
unknown <francois.leclercq@drp-software.fr>
2024-03-15 14:02:00 +0100

    écriture fonction de conversion + test unitaire

--- src/Controller/FunctionCvtToRom.js
+++ src/Controller/FunctionCvtToRom.js
@@ -8,7 +8,26 @@
 */
 
 function CvtToRom(nbr) {
-  return null;
+  let result = "";
+
+  const listRules = [
+    { value: 1000, rule: "M" },
+    { value: 500, rule: "D" },
+    { value: 100, rule: "C" },
+    { value: 50, rule: "L" },
+    { value: 10, rule: "X" },
+    { value: 5, rule: "V" },
+    { value: 1, rule: "I" },
+  ];
+
+  for (let i = 0; i < listRules.length; i++) {
+    while (nbr >= listRules[i].value) {
+      result = result + listRules[i].rule;
+      nbr = nbr - listRules[i].value;
+    }
+  }
+  console.log(result);
+  return result;
 }
 
 export default CvtToRom;
--- src/Test/Interface.test.js
+++ src/Test/Interface.test.js
@@ -1,6 +1,10 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import Interface from "../Components/Interface";
 
+//Import interfaces et fonctions
+import Interface from "../Components/Interface";
+import CvtToRom from "../Controller/FunctionCvtToRom";
+
+//Vérifie l'existance de mes composants dans mon Interface
 test("renders check elements", () => {
   render(<Interface />);
 
@@ -15,9 +19,32 @@ test("renders check elements", () => {
   expect(BtnCvtRom).toBeInTheDocument();
 });
 
+//Vérifie si ma valeur initial a convertir est bien à 0 avant insertion
 test("renders check initial value", () => {
   render(<Interface />);
   const textField = screen.getByTestId("numberToCvt");
   const textFieldValue = textField.querySelector("input").value;
   expect(textFieldValue).toBe("0");
+});
+
+//Vérifie jusqu'a 10 fois le test de ma fonction de conversion Romaine
+test("Test de la fonction de conversion du numérique au format romain jusqu'à 10", () => {
+  //liste des cas qui doit étre validé
+  const testCases = [
+    { value: 1, rule: "I" },
+    { value: 2, rule: "II" },
+    { value: 3, rule: "III" },
+    { value: 4, rule: "IV" },
+    { value: 5, rule: "V" },
+    { value: 6, rule: "VI" },
+    { value: 7, rule: "VII" },
+    { value: 8, rule: "VIII" },
+    { value: 9, rule: "IX" },
+    { value: 10, rule: "X" },
+  ];
+
+  testCases.forEach(({ value, rule }) => {
+    const result = CvtToRom(value); // Appel de la fonction de conversion
+    expect(result).toBe(rule); // Vérification du résultat
+  });
 });
